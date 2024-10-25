5c410e17b08056de6b7b5117997ec8e33457694f
Riddhi Suteri <ridsuteri@gmail.com>
2024-10-25 09:56:19 +0530

    react folder structure

---
+++ react/basic/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     Hello World, I am learning react js
+    </>
+  )
+}
+
+export default App
