462ae466c8c45a938610ec35ffad1c3d380b58fc
Riddhi Suteri <ridsuteri@gmail.com>
2024-11-02 22:56:16 +0530

    add product list

--- react/product-list/src/Product.jsx
+++ react/product-list/src/Product.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Product = (props) => {
+  const [addToCart, setAddToCart] = useState(false);
   return (
     <div>
-        <h4>{props.product.name}</h4>
-        <h5>{props.product.price}</h5>
-        <hr />
-    </div>
-  )
-}
+      <h4>{props.product.name}</h4>
+      <h5>{props.product.price}</h5>
+      {addToCart ? (
+        <button
+          onClick={() => {
+            setAddToCart(!addToCart);
+          }}
+        >
+          Added to Cart
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setAddToCart(!addToCart);
+          }}
+        >
+          Add to Cart
+        </button>
+      )}
 
-export default Product
+      <hr />
+    </div>
+  );
+};
+
+export default Product;
--- react/product-list/src/ProductList.jsx
+++ react/product-list/src/ProductList.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Product from "./Product";
-import products from './mockData/products.json';
+import products from "./mockData/products.json";
 
 const ProductList = () => {
-//   let products = [
-//     { name: "product1", price: 1000 },
-//     { name: "product2", price: 2000 },
-//   ];
+  //   let products = [
+  //     { name: "product1", price: 1000 },
+  //     { name: "product2", price: 2000 },
+  //   ];
   return (
-  <div>
-    <h2>Product List</h2>
-    {
-        products.map((product)=>(
-            <Product product={product} />
-        ))
-    }
-  </div>
+    <div>
+      <h2>Product List</h2>
+      {products.map((product) => (
+        <Product product={product}  />
+      ))}
+    </div>
   );
 };
 
