02a610b53e384d57eef70433ffa972b9268288ee
Riddhi Suteri <ridsuteri@gmail.com>
2024-12-31 14:45:54 +0530

    add mongodb setup

--- node/express/view-engines/server.js
+++ node/express/view-engines/server.js
@@ -1,19 +1,30 @@
+import express from 'express';
+import userRoutes from './routes/userRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
+import { MongoClient } from 'mongodb';
 
-const express = require('express')
-const userRoutes = require('./routes/userRoutes')
-const bookRoutes = require('./routes/bookRoutes')
+const connectionString = 'mongodb://localhost:27017/riddhi-test';
+const client = new MongoClient(connectionString);
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+let connection;
+(async () => {
+  try {
+    connection = await client.connect();
+    console.log('db connected successfully!');
+  } catch (e) {
+    console.error('Error connecting to database:', e);
+  }
+})();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.use('/users', userRoutes)
-app.use('/books', bookRoutes)
-
+app.use('/users', userRoutes);
+app.use('/books', bookRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
